deeb88ba8936f657d4a41968e106a05e31a14850
Artem <kypidon71@gmail.com>
2022-09-14 22:45:47 +0300

    add random

--- HelloConsol003/Program.cs
+++ HelloConsol003/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 3; // int(значение целого числа)
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // add random число
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.WriteLine(numberA + numberB);
+Console.Write("your resultat ");
 Console.WriteLine(result);
