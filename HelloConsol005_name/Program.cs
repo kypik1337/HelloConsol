173b46c7ffa8c812ef11f149e57c482bc385979a
Artem <kypidon71@gmail.com>
2022-09-14 23:08:43 +0300

    add name program

---
+++ HelloConsol005_name/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" Write your Name");
+string username = Console.ReadLine();
+if(username == "Artem")
+    {
+        Console.WriteLine("Hi Brooooooo!!!");
+    }
+else
+   { 
+        Console.WriteLine("Who are you, ");
+        Console.WriteLine(username);
+   }
